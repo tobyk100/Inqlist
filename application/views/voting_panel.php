fb86b222aa34ebe8b6697e77a67cf176b3d208bb
Tobias Kahan <tobyk100@gmail.com>
2012-11-30 13:30:40 -0800

    changed to fixed

--- application/views/voting_panel.php
+++ application/views/voting_panel.php
@@ -2,8 +2,11 @@
   <div class="text">
     The plots of land adjacent to the highways of LA are bought
     up and used only for billboards, providing
-    no benefit to the community. wha
+    no benefit to the community. wha long long long
+    lonngngngngngg
   </div>
-  <div id="solution-slider"></div>
-  <div id="urgency-slider"></div>
+  <div id="sliders">
+    <span>Solved</span><div id="solution-slider"></div><span>Unsolved</span>
+    <span>Trivial</span><div id="urgency-slider"></div><span>Urgent</span>
+  </div>
 </li>
