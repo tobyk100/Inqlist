7d20af631f939bbdf5fefc62ffd322797a97de5e
Tobias Kahan <tobyk100@gmail.com>
2012-11-30 07:18:42 -0800

    more ajax

--- application/controllers/main.php
+++ application/controllers/main.php
@@ -5,6 +5,8 @@ class Main extends CI_Controller {
   {
     parent::__construct();
     $this->load->model('inqling_model');
+    $this->load->model('solution_model');
+    $this->load->model('comment_model');
   }
 	public function index()
 	{
@@ -21,10 +23,16 @@ class Main extends CI_Controller {
     $new_inqling = $this->inqling_model->create_inqling($body);
     $this->output->set_output(json_encode($new_inqling));
   }
-  public function create_comment() {
-    
-  }
   public function create_solution() {
-    
+    $body = $this->input->post('body');
+    $inqling_id = $this->input->post('inqling');
+    $new_solution = $this->->solution_model->create_solution($body, $inqling_id);
+    $this->output->set_output(json_encode($new_solution));
+  }
+  public function create_comment() {
+    $body = $this->input->post('body');
+    $inqling_id = $this->input->post('inqling');
+    $new_solution = $this->->solution_model->create_solution($body, $inqling_id);
+    $this->output->set_output(json_encode($new_solution));
   }
 }
--- public_html/assets/javascript/inqling.js
+++ public_html/assets/javascript/inqling.js
@@ -12,25 +12,28 @@ add_inqling = function() {
 create_solution = function() {
   $.ajax({
     type: 'POST',
-    url: '/index.php/main/create_inqling',
+    url: '/index.php/main/create_solution',
     data: {
-      'body': 'comment body', 
+      'body': 'solution body', 
       'inqling': '1'
     },
     dataType: 'json',
     success: function(data) {
       alert(data);
     }
-  }
+  });
 }
 create_comment = function() {
   $.ajax({
     type: 'POST',
-    url: '/index.php/main/create_inqling',
-    data: {'body': 'comment body' },
+    url: '/index.php/main/create_comment',
+    data: {
+      'body': 'comment body',
+      'comment': '1'
+    },
     dataType: 'json',
     success: function(data) {
       alert(data);
     }
-  }
+  });
 }
