3b57751774d7cf40077a821e10cab930efd10d5d
Tobias Kahan <tobyk100@gmail.com>
2012-11-30 12:46:33 -0800

    pulled out panel

---
+++ application/views/voting_panel.php
@@ -0,0 +1,9 @@
+<li class="voting-panel active">
+  <div class="text">
+    The plots of land adjacent to the highways of LA are bought
+    up and used only for billboards, providing
+    no benefit to the community. wha
+  </div>
+  <div id="solution-slider"></div>
+  <div id="urgency-slider"></div>
+</li>
--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -18,22 +18,24 @@
 </header>
 
 <div id="main" class="container">
-  
-  <!--<h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</h1>-->
 
   <ul class="sort">
     <li class="active">Trending</li>
     <li>All Time</li>
-    <li>New</li>    
+    <li>New</li>
   </ul>
 
-  <li class="voting-panel active"><div class="text">The plots of land adjacent to the highways of LA are bought up and used only for billboards, providing no benefit to the community.</div><div id="solution-slider"></div><div id="urgency-slider"></div></li>  
-  <ul class="comments">
-    
-  </ul>
+  <?php $this->load->view('voting_panel'); ?>
+  <ul class="comments"></ul>
   <ul class="posts">
     <?php foreach($inqlings as $inqling){ ?>
-      <li id = '<?php echo "inq_" . $inqling['id']; ?>'><div class="text"><?php echo $inqling['body']; ?> </div> <div class="vote-bg"><span class="vote-text"><?php echo number_format($inqling['pub_rating'], 1); ?></span></div> <img src="/assets/images/user.png"></li>  
+      <li id = '<?php echo "inq_" . $inqling['id']; ?>'>
+        <div class="text"><?php echo $inqling['body']; ?></div>
+        <div class="vote-bg">
+          <span class="vote-text"><?php echo number_format($inqling['pub_rating'], 1); ?></span>
+        </div>
+         <img src="/assets/images/user.png">
+      </li>
     <?php } ?>
   </ul>
 </div>
