0e24bfea00e6976b1f78e95a0a442f212cd95fed
Tobias Kahan <tobyk100@gmail.com>
2012-11-30 03:43:12 -0800

    mysql dump

---
+++ dump.sql
@@ -0,0 +1,133 @@
+-- MySQL dump 10.13  Distrib 5.5.24, for Linux (x86_64)
+--
+-- Host: localhost    Database: inqlist
+-- ------------------------------------------------------
+-- Server version	5.5.24
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `comments`
+--
+
+DROP TABLE IF EXISTS `comments`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `comments` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `body` varchar(255) DEFAULT NULL,
+  `date_created` datetime DEFAULT NULL,
+  `s_id` int(11) DEFAULT NULL,
+  `u_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `s_id` (`s_id`),
+  KEY `u_id` (`u_id`),
+  CONSTRAINT `comments_ibfk_1` FOREIGN KEY (`s_id`) REFERENCES `solutions` (`id`),
+  CONSTRAINT `comments_ibfk_2` FOREIGN KEY (`u_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `inqlings`
+--
+
+DROP TABLE IF EXISTS `inqlings`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `inqlings` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `body` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `url` varchar(255) DEFAULT NULL,
+  `date_created` datetime DEFAULT NULL,
+  `u_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `u_id` (`u_id`),
+  CONSTRAINT `inqlings_ibfk_1` FOREIGN KEY (`u_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `solutions`
+--
+
+DROP TABLE IF EXISTS `solutions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `solutions` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `body` varchar(255) DEFAULT NULL,
+  `up_votes` int(11) DEFAULT NULL,
+  `down_votes` int(11) DEFAULT NULL,
+  `iapp_flag` tinyint(1) DEFAULT NULL,
+  `date_created` datetime DEFAULT NULL,
+  `u_id` int(11) DEFAULT NULL,
+  `inq_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `u_id` (`u_id`),
+  KEY `inq_id` (`inq_id`),
+  CONSTRAINT `solutions_ibfk_1` FOREIGN KEY (`u_id`) REFERENCES `users` (`id`),
+  CONSTRAINT `solutions_ibfk_2` FOREIGN KEY (`inq_id`) REFERENCES `inqlings` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `url` varchar(255) DEFAULT NULL,
+  `date_created` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `votes`
+--
+
+DROP TABLE IF EXISTS `votes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `votes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `solution` int(11) DEFAULT NULL,
+  `urgency` int(11) DEFAULT NULL,
+  `u_id` int(11) DEFAULT NULL,
+  `inq_id` int(11) DEFAULT NULL,
+  `date_creted` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `u_id` (`u_id`),
+  KEY `inq_id` (`inq_id`),
+  CONSTRAINT `votes_ibfk_1` FOREIGN KEY (`u_id`) REFERENCES `users` (`id`),
+  CONSTRAINT `votes_ibfk_2` FOREIGN KEY (`inq_id`) REFERENCES `inqlings` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2012-11-30  3:39:48
