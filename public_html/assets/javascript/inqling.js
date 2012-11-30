1fbc35f7586bf5ceacd6fa1eab3294d51242f53c
Tobias Kahan <tobyk100@gmail.com>
2012-11-30 06:55:51 -0800

    more ajax

--- public_html/assets/javascript/inqling.js
+++ public_html/assets/javascript/inqling.js
@@ -9,3 +9,25 @@ add_inqling = function() {
     }
   });
 }
+create_solution = function() {
+  $.ajax({
+    type: 'POST',
+    url: '/index.php/main/create_inqling',
+    data: {'body': 'comment body' },
+    dataType: 'json',
+    success: function(data) {
+      alert(data);
+    }
+  }
+}
+create_comment = function() {
+  $.ajax({
+    type: 'POST',
+    url: '/index.php/main/create_inqling',
+    data: {'body': 'comment body' },
+    dataType: 'json',
+    success: function(data) {
+      alert(data);
+    }
+  }
+}
