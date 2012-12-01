3152fd5f4ef12218c8e9bc9fedb35fc6641771f5
Carlo Valentin <me@carlovalentin.com>
2012-12-01 15:42:32 -0800

    InqulingModel fixes
    fixed getting posts and edited methods

--- application/models/Inquling_Model.php
+++ application/models/Inquling_Model.php
@@ -28,13 +28,13 @@ class Inqling_Model extends CI_Model{
 		#Urgency 0-50, Solution 0-100
 		#overall rating = urgency * solution
 		$query = $this->db->order_by("overall_rating", ASC);
-		$query = $this->db->get(/*inqling table placeholder*/,$inq_start, $inq_num);			
+		$query = $this->db->get(/*inqling table placeholder*/,$inq_num, $inq_start);			
 	}
 
 	//Gets new inquling based on date
 	public function get_new_inqulings($inq_start = 0, $inq_num = 10){
 		$query = $this->db->order_by("date", ASC);
-		$query = $this->db->get(/*inqling table placeholder*/,$inq_start, $inq_num);		
+		$query = $this->db->get(/*inqling table placeholder*/,$inq_num, $inq_start);		
 	}
 
 	//Gets top inqulings based on algorithm that we do not know
@@ -42,13 +42,15 @@ class Inqling_Model extends CI_Model{
 		#Urgency 0-50, Solution 0-100
 		#overall rating = urgency * solution
 		$query = $this->db->order_by("overall_rating", ASC);
-		$query = $this->db->get(/*inqling table placeholder*/,$inq_start, $inq_num);			
+		$query = $this->db->get(/*inqling table placeholder*/,$inq_num, $inq_start);			
 	}
 
 	//Gets trendign inqulings based on algo that we don't know either
 	public function get_trending_inqulings($inq_id = -1){
-		$query = $this->db->order_by("trend_rank", ASC);
-		$query = $this->db->get(/*inqling table placeholder*/,$inq_start, $inq_num);
+		$query = $this->db->get_where('votes',2,0);
+		foreach($query->result_array as $row){
+			//do magic
+		}
 	}
 }
 
