ffeb06b097b462d47a0ae2b53afc3ea2f79ed43a
Carlo Valentin <me@carlovalentin.com>
2012-12-02 10:00:26 -0800

    css add

--- application/views/header.php
+++ application/views/header.php
@@ -4,6 +4,7 @@
   <link rel="stylesheet" type="text/css" href="/assets/css/core.css"/>
   <link rel="stylesheet" type="text/css" href="/assets/css/home.css"/>
   <link rel="stylesheet" type="text/css" href="/assets/css/jquery-ui.css"/>
+  <link rel="stylesheet" type="text/css" href="/assets/css/vote.css"/>
 
   <script type="text/javascript" src="/assets/javascript/jquery.js"></script>
   <script type="text/javascript" src="/assets/javascript/jquery-ui.js"></script>
