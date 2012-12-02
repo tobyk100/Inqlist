5e1701dc2ecdeda58b5041a9e22e8560208ee224
Brandon <brandoncapecci@gmail.com>
2012-12-01 21:25:24 -0800

    Brandon HTML

--- application/views/header.php
+++ application/views/header.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <link rel="stylesheet" type="text/css" href="assets/css/core.css"/>
-  <link rel="stylesheet" type="text/css" href="assets/css/home.css"/>
+  <link rel="stylesheet" type="text/css" href="/assets/css/core.css"/>
+  <link rel="stylesheet" type="text/css" href="/assets/css/home.css"/>
 
-  <script type="text/javascript" src="assets/javascript/jquery.js"></script>
-  <script type="text/javascript" src="assets/javascript/home.js"></script>
+  <script type="text/javascript" src="/assets/javascript/jquery.js"></script>
+  <script type="text/javascript" src="/assets/javascript/home.js"></script>
 
 </head>
--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -2,52 +2,61 @@
 
 <header>
   <div class="container">
-    <img id="logo" src="assets/images/logo.png" />
+    <img id="logo" src="/assets/images/logo.png" />
     <input id="search" value="Search an Idea or Person">
     <ul>
       <li>Write Anonymously</li>
       <li>Help</li>
-      <li>Filter</li>
+      <li>Filter
+        <div id="dropdown">
+        Hello
+        </div>
+      </li>
       <li>Login</li>
     <ul>
   </div>  
 </header>
 
 <div id="main" class="container">
+  
+  <h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</h1>
+
+  <ul class="sort">
+    <li class="active">Trending</li>
+    <li>All Time</li>
+    <li>New</li>    
+  </ul>
+
   <ul class="posts">
-    <li><div class="text">"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."</div> <div class="vote-bg"><span class="vote-text">9.2</span></div> <img src="assets/images/user.png"></li>  
+    <li><div class="text">"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."</div> <div class="vote-bg"><span class="vote-text">9.2</span></div> <img src="/assets/images/user.png"></li>  
       <ul class="comments">
-        <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."</li>
-        <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."</li>
-        <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."</li>
-        <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."</li>
-        <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."</li>
+        <li><div class="comment">"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."</div><div class="arrow-up">&#x25B2;</div><div class="arrow-down">&#x25BC;</div></li>
+        <li><div class="comment">"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."</div><div class="arrow-up">&#x25B2;</div><div class="arrow-down">&#x25BC;</div></li>
+        <li><div class="comment">"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."</div><div class="arrow-up">&#x25B2;</div><div class="arrow-down">&#x25BC;</div></li>
+        <li><div class="comment">"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."</div><div class="arrow-up">&#x25B2;</div><div class="arrow-down">&#x25BC;</div></li>
+        <li><div class="comment">"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."</div><div class="arrow-up">&#x25B2;</div><div class="arrow-down">&#x25BC;</div></li>
+        <li><div class="comment">"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."</div><div class="arrow-up">&#x25B2;</div><div class="arrow-down">&#x25BC;</div></li>
       </ul>
 
-    <li><div class="text">"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."</div> <div class="vote-bg"><span class="vote-text">9.2</span></div> <img src="assets/images/user.png"></li>  
-    <li><div class="text">"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."</div> <div class="vote-bg"><span class="vote-text">9.2</span></div> <img src="assets/images/user.png"></li>  
-    <li><div class="text">"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."</div> <div class="vote-bg"><span class="vote-text">9.2</span></div> <img src="assets/images/user.png"></li>  
-    <li><div class="text">"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."</div> <div class="vote-bg"><span class="vote-text">9.2</span></div> <img src="assets/images/user.png"></li>  
-    <li><div class="text">"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."</div> <div class="vote-bg"><span class="vote-text">9.2</span></div> <img src="assets/images/user.png"></li>  
-    <li><div class="text">"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."</div> <div class="vote-bg"><span class="vote-text">9.2</span></div> <img src="assets/images/user.png"></li>  
-    <li><div class="text">"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."</div> <div class="vote-bg"><span class="vote-text">9.2</span></div> <img src="assets/images/user.png"></li>  
-    <li><div class="text">"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."</div> <div class="vote-bg"><span class="vote-text">9.2</span></div> <img src="assets/images/user.png"></li>  
-    <li><div class="text">"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."</div> <div class="vote-bg"><span class="vote-text">9.2</span></div> <img src="assets/images/user.png"></li>  
-    <li><div class="text">"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."</div> <div class="vote-bg"><span class="vote-text">9.2</span></div> <img src="assets/images/user.png"></li>  
-    <li><div class="text">"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."</div> <div class="vote-bg"><span class="vote-text">9.2</span></div> <img src="assets/images/user.png"></li>  
-
-    <li><div class="text">"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."</div> <div class="vote-bg"><span class="vote-text">9.2</span></div> <img src="assets/images/user.png"></li>  
-    <li><div class="text">"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."</div> <div class="vote-bg"><span class="vote-text">9.2</span></div> <img src="assets/images/user.png"></li>  
-    <li><div class="text">"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."</div> <div class="vote-bg"><span class="vote-text">9.2</span></div> <img src="assets/images/user.png"></li>  
-    <li><div class="text">"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."</div> <div class="vote-bg"><span class="vote-text">9.2</span></div> <img src="assets/images/user.png"></li>  
-    <li><div class="text">"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."</div> <div class="vote-bg"><span class="vote-text">9.2</span></div> <img src="assets/images/user.png"></li>  
-    <li><div class="text">"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."</div> <div class="vote-bg"><span class="vote-text">9.2</span></div> <img src="assets/images/user.png"></li>  
-    <li><div class="text">"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."</div> <div class="vote-bg"><span class="vote-text">9.2</span></div> <img src="assets/images/user.png"></li>  
-    <li><div class="text">"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."</div> <div class="vote-bg"><span class="vote-text">9.2</span></div> <img src="assets/images/user.png"></li>  
-    <li><div class="text">"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."</div> <div class="vote-bg"><span class="vote-text">9.2</span></div> <img src="assets/images/user.png"></li>  
-    <li><div class="text">"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."</div> <div class="vote-bg"><span class="vote-text">9.2</span></div> <img src="assets/images/user.png"></li>  
-    <li><div class="text">"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."</div> <div class="vote-bg"><span class="vote-text">9.2</span></div> <img src="assets/images/user.png"></li>  
-  
+    <li><div class="text">"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."</div> <div class="vote-bg"><span class="vote-text">9.2</span></div> <img src="/assets/images/user.png"></li>  
+    <li><div class="text">"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."</div> <div class="vote-bg"><span class="vote-text">9.2</span></div> <img src="/assets/images/user.png"></li>  
+    <li><div class="text">"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."</div> <div class="vote-bg"><span class="vote-text">9.2</span></div> <img src="/assets/images/user.png"></li>  
+    <li><div class="text">"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."</div> <div class="vote-bg"><span class="vote-text">9.2</span></div> <img src="/assets/images/user.png"></li>  
+    <li><div class="text">"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."</div> <div class="vote-bg"><span class="vote-text">9.2</span></div> <img src="/assets/images/user.png"></li>  
+    <li><div class="text">"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."</div> <div class="vote-bg"><span class="vote-text">9.2</span></div> <img src="/assets/images/user.png"></li>  
+    <li><div class="text">"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."</div> <div class="vote-bg"><span class="vote-text">9.2</span></div> <img src="/assets/images/user.png"></li>  
+    <li><div class="text">"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."</div> <div class="vote-bg"><span class="vote-text">9.2</span></div> <img src="/assets/images/user.png"></li>  
+    <li><div class="text">"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."</div> <div class="vote-bg"><span class="vote-text">9.2</span></div> <img src="/assets/images/user.png"></li>  
+    <li><div class="text">"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."</div> <div class="vote-bg"><span class="vote-text">9.2</span></div> <img src="/assets/images/user.png"></li>  
+    <li><div class="text">"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."</div> <div class="vote-bg"><span class="vote-text">9.2</span></div> <img src="/assets/images/user.png"></li>  
+    <li><div class="text">"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."</div> <div class="vote-bg"><span class="vote-text">9.2</span></div> <img src="/assets/images/user.png"></li>  
+    <li><div class="text">"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."</div> <div class="vote-bg"><span class="vote-text">9.2</span></div> <img src="/assets/images/user.png"></li>  
+    <li><div class="text">"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."</div> <div class="vote-bg"><span class="vote-text">9.2</span></div> <img src="/assets/images/user.png"></li>  
+    <li><div class="text">"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."</div> <div class="vote-bg"><span class="vote-text">9.2</span></div> <img src="/assets/images/user.png"></li>  
+    <li><div class="text">"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."</div> <div class="vote-bg"><span class="vote-text">9.2</span></div> <img src="/assets/images/user.png"></li>  
+    <li><div class="text">"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."</div> <div class="vote-bg"><span class="vote-text">9.2</span></div> <img src="/assets/images/user.png"></li>  
+    <li><div class="text">"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."</div> <div class="vote-bg"><span class="vote-text">9.2</span></div> <img src="/assets/images/user.png"></li>  
+    
 
   </ul>
 </div>
