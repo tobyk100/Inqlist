7dd3d758595976d2415c4ceae24ef2be8d334b20
Carlo Valentin <me@carlovalentin.com>
2012-12-02 01:29:25 -0800

    solutions.js

---
+++ public_html/assets/javascript/solutions.js
@@ -0,0 +1,13 @@
+disp_solutions = function() {
+  $.ajax({
+    type: 'POST',
+    url: '/index.php/main/get_solutions',
+    data: {'id': $('this').attr('id')},
+    dataType: 'json',
+    success: function(data) {
+      $.each(data, function(row, solution){
+        $(<li><div class="comment">solution.body</div><div class="arrow-up">&#x25B2;</div><div class="arrow-down">&#x25BC;</div></li>).appendTo("#solutions");
+      })
+    }
+  });
+}
